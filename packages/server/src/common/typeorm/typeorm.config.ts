568ecf4b365bcf78d8ea0af446fcdbad3166b7de
AllenHUSH <allenshhu@outlook.com>
2025-09-23 00:47:38 +0800

    feat(数据库): 从PostgreSQL切换至MySQL并更新相关配置
    修改数据库配置以支持MySQL，包括添加mysql2依赖和更新TypeORM配置
    设置开发环境自动同步数据库结构
    添加NODE_ENV环境变量到启动脚本

--- packages/server/src/common/typeorm/typeorm.config.ts
+++ packages/server/src/common/typeorm/typeorm.config.ts
@@ -1,12 +1,21 @@
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
-  type: 'postgres',
-  url: configService.get('DATABASE_URL'),
+export const typeOrmConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
+  type: 'mysql',
+  host: configService.get('DATABASE_HOST'),
+  port: configService.get('DATABASE_PORT'),
+  username: configService.get('DATABASE_USERNAME'),
+  password: configService.get('DATABASE_PASSWORD'),
+  database: configService.get('DATABASE_NAME'),
   entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
-  synchronize: false, // 生产环境应设置为false
+  synchronize: configService.get('NODE_ENV') === 'development',
   logging: configService.get('NODE_ENV') === 'development',
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   migrationsTableName: 'migrations',
+  extra: {
+    allowPublicKeyRetrieval: true,
+  },
 });
