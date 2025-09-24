173656011652d2e4cc8e0c3a585b7c2a61b150e2
AllenHUSH <allenshhu@outlook.com>
2025-09-24 22:17:18 +0800

    refactor(login): 将表单部分提取为独立组件以提升可维护性

--- packages/web/src/views/Login/components/login-form.tsx
+++ packages/web/src/views/Login/components/login-form.tsx
@@ -1,11 +1,8 @@
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { toast } from "sonner"
 import { useState, type FC } from "react"
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { OAuthPart } from "./oauth-part"
+import { FormPart } from "./form-part"
 
 interface IProps {
   className?: string
@@ -21,12 +18,6 @@ export const LoginForm: FC<IProps> = ({
     setTab(value as 'password' | 'verification-code')
   }
 
-  const handleClickForgotPassword = () => {
-    toast('忘记密码实现中', {
-      description: '敬请期待',
-    })
-  }
-
   return (
     <div className={cn("flex flex-col gap-6", className)}>
       <Tabs value={tab} onValueChange={handleChangeTab} >
@@ -38,27 +29,7 @@ export const LoginForm: FC<IProps> = ({
 
       <div className="grid gap-6">
         <div className="grid gap-3">
-          <div className="grid gap-3">
-            <Label htmlFor="email">邮箱</Label>
-            <Input id="email" type="email" placeholder="m@example.com" required />
-          </div>
-          <div className="grid gap-3">
-            <div className="flex items-center">
-              <Label htmlFor="password">密码</Label>
-              <a
-                href="#"
-                className="ml-auto text-sm underline-offset-4 hover:underline"
-                onClick={handleClickForgotPassword}
-              >
-                忘记密码？
-              </a>
-            </div>
-            <Input id="password" type="password" required />
-          </div>
-          <Button type="submit" className="w-full gap-3">
-            登录
-          </Button>
-
+          <FormPart mode={tab} />
           <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
             <span className="bg-background text-muted-foreground relative z-10 px-2">
               或者使用
